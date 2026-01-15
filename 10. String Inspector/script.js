86aa3268bf7fc62192ad228611498b3c0bdb66b6
Ankieta <ankietathorve@gmail.com>
2026-01-15 15:10:21 +0530

    Final console

--- 10. String Inspector/script.js
+++ 10. String Inspector/script.js
@@ -24,3 +24,4 @@ console.log(`The first word is "${greetingWord}".`);
 const endPunctuation = message.slice(-1);
 console.log(`The ending punctuation mark is a "${endPunctuation}"`);
 
+console.log("Workshop complete! You now know how to use includes() and slice().");
