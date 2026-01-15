40a0f57c62ac1c57c3369fa0afafe2bd95b82086
Ankieta <ankietathorve@gmail.com>
2026-01-15 23:25:34 +0530

    Update trimEnd()

--- 11. String Formatter/script.js
+++ 11. String Formatter/script.js
@@ -10,3 +10,7 @@ const trimmedStart = userInput.trimStart();
 console.log("After using the trimStart() method, leading spaces removed:");
 console.log(trimmedStart);
 
+const trimmedEnd = userInput.trimEnd();
+console.log("After using the trimEnd() method, trailing spaces removed:");
+console.log(trimmedEnd);
+
