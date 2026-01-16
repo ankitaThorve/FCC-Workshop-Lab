8bfd682f433c71eb53e0bb055a3b8b23a0d6e883
Ankieta <ankietathorve@gmail.com>
2026-01-16 19:06:02 +0530

    pre-increment

--- 12. Inc-Decre Operators/script.js
+++ 12. Inc-Decre Operators/script.js
@@ -1,23 +1,23 @@
-// let counter = 10;
-// console.log(`Counter is currently ${counter}`);
+let counter = 10;
+console.log(`Counter is currently ${counter}`);
 
-// let updatedCounter = ++counter;
-// console.log(`Counter is now ${updatedCounter}`);
+let updatedCounter = ++counter; //pre-increment
+console.log(`Counter is now ${updatedCounter}`);
 
 // let score = 8;
 // console.log(`Score is currently ${score}`);
 
-// let finalScore = score++;
+// let finalScore = score++; //post-increment
 // console.log(`Score is still ${finalScore}`);
 
 // let coins = 3;
 // console.log(`Coins is currently ${coins}`);
 
-// let updatedCoins = --coins;
+// let updatedCoins = --coins; //pre-decrement
 // console.log(`Coins is now ${updatedCoins}`);
 
 // let health = 7;
 // console.log(`Health is currently ${health}`);
 
-// let newHealth = health--;
+// let newHealth = health--; //post-decrement
 // console.log(`Health is still ${newHealth}`);
