a3920feaf55e217929cb1257a2780d8e6afb54c2
Ankieta <ankietathorve@gmail.com>
2026-01-19 13:29:09 +0530

    Math.max()

--- 14. Mathbot/script.js
+++ 14. Mathbot/script.js
@@ -47,3 +47,7 @@ console.log(numRounded);
 const numRounded2 = Math.round(11.2);
 console.log(numRounded2);
 
+console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.");
+//Math.max() ------
+const maxNum = Math.max(3, 125, 55, 24);
+console.log(maxNum);
