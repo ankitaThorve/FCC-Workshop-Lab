85b7226a0d023f66d51c9b613ffe484f2f4f1683
Ankieta <ankietathorve@gmail.com>
2026-01-20 12:28:13 +0530

    Calculate sum of two nums using regular function

--- 16. Calculator/script.js
+++ 16. Calculator/script.js
@@ -1,11 +1,11 @@
+//Regular function
 function addTwoAndSeven() {
   return 2 + 7;
 }
-
 console.log(addTwoAndSeven());
 
+//Another regular function
 function addThreeAndFour() {
   return 3 + 4;
 }
-
 console.log(addThreeAndFour());
