7d932014bfb9f2747fb81f0775ab967deefe00df
Ankieta <ankietathorve@gmail.com>
2026-01-20 12:37:07 +0530

    subtract two numbers

--- 16. Calculator/script.js
+++ 16. Calculator/script.js
@@ -19,3 +19,12 @@ function calculateSum(num1, num2) { //function definition
 console.log(calculateSum(2,5));
 console.log(calculateSum(10, 10));
 console.log(calculateSum(5, 5));
+
+//subtract two numbers
+function calculateDifference(num1, num2){
+  return num1 - num2;
+}
+
+console.log(calculateDifference(22,5));
+console.log(calculateDifference(12,1));
+console.log(calculateDifference(17,9));
