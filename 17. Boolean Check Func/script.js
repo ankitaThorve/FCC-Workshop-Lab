0c4f8725138663630c2d940458fba6faccb07818
Ankieta <ankietathorve@gmail.com>
2026-01-20 16:03:33 +0530

    Function checks boolean values

---
+++ 17. Boolean Check Func/script.js
@@ -0,0 +1,14 @@
+function booWho(boo){
+  return typeof boo === "boolean";
+}
+
+console.log(booWho(true)) //should return true
+console.log(booWho(false)) //should return true. 
+console.log(booWho([1, 2, 3])) //should return false. 
+console.log(booWho([].slice)) //should return false. 
+console.log(booWho({ "a": 1 })) //should return false. 
+console.log(booWho(1)) //should return false. 
+console.log(booWho(NaN)) //should return false. 
+console.log(booWho("a")) //should return false. 
+console.log(booWho("true")) //should return false
+console.log(booWho("false")) //should return false.
