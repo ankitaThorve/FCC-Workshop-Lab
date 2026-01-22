82e708a17079c38c45b1f7b7726ece38a7201d07
Ankieta <ankietathorve@gmail.com>
2026-01-22 22:58:26 +0530

    Build a Celsius to Fahrenheit Converter

---
+++ 20. CelsToFahr Converter/script.js
@@ -0,0 +1,11 @@
+//Build a Celsius to Fahrenheit Converter
+function convertCtoF(tempInCelsius){
+  return tempInCelsius * (9/5) + 32;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30))
+console.log(convertCtoF(-10))
+console.log(convertCtoF(0))
+console.log(convertCtoF(20))
+console.log(convertCtoF(30))
