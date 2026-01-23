86cefaaf71ca86c0601cdaf7753c5364a7945c71
Ankieta <ankietathorve@gmail.com>
2026-01-23 22:39:38 +0530

    Console card argument

--- 21. Card Counting/script.js
+++ 21. Card Counting/script.js
@@ -18,3 +18,17 @@ function cardCounter(card) {
   // Cards 7, 8, 9 do not affect the count
   return count > 0 ? count + " Bet" : count + " Hold"; // Decide action
 }
+
+console.log(cardCounter(2));
+console.log(cardCounter(3));
+console.log(cardCounter(4));
+console.log(cardCounter(5));
+console.log(cardCounter(6));
+console.log(cardCounter(7));
+console.log(cardCounter(8));
+console.log(cardCounter(9));
+console.log(cardCounter(10));
+console.log(cardCounter("J"));
+console.log(cardCounter("Q"));
+console.log(cardCounter("K"));
+console.log(cardCounter("A"));
