342b44cfa3671e54e679a07298991d0a9c0ce26f
Ankieta <ankietathorve@gmail.com>
2026-01-23 22:37:27 +0530

    Decrease count for high cards

--- 21. Card Counting/script.js
+++ 21. Card Counting/script.js
@@ -1,11 +1,19 @@
 // Global variable
 let count = 0;
 
-function cardCounter(card){
+function cardCounter(card) {
   // Increase count for low cards
-  if(card >= 2 && card<= 6){
+  if (card >= 2 && card <= 6) {
     count++;
+  } // Decrease count for high cards
+  else if (
+    card === 10 ||
+    card === "J" ||
+    card === "Q" ||
+    card === "K" ||
+    card === "A"
+  ) {
+    count--;
   }
 }
-
 
