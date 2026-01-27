4600ba0a475febe2bde40684e812e3bf205bcdf9
Ankieta <ankietathorve@gmail.com>
2026-01-27 19:01:38 +0530

    Create a function with if else conditions to check leap year

--- 23. Leap Year Calculator/script.js
+++ 23. Leap Year Calculator/script.js
@@ -1,1 +1,13 @@
 let year = 2000;
+function isLeapYear(year){
+  if(year % 400 === 0){
+    return `${year} is a leap year.`;
+  } else if (year % 100 === 0){
+    return `${year} is not a leap year.`;
+  } else if(year % 4 === 0){
+    return `${year} is a leap year.`;
+  } else{
+    return `${year} is not a leap year.`;
+  }
+}
+
