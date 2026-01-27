393a78bcedb0a372efee47ac9ef4ea526619c36f
Ankieta <ankietathorve@gmail.com>
2026-01-27 19:10:53 +0530

    Console function with diff arguments

--- 24. Truncated String/script.js
+++ 24. Truncated String/script.js
@@ -5,3 +5,11 @@ function truncateString(str, num){ //inputs 2 - string, number
   }
   return str; //If condition is false, return original string
 }
+
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11))
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length))
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2))
+console.log(truncateString("A-", 1))
+console.log(truncateString("Absolutely Longer", 2))
