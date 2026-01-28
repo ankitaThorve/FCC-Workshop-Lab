b68b7a90ac53cef5530acba2d8206a18bdcbeeb6
Ankieta <ankietathorve@gmail.com>
2026-01-28 21:42:51 +0530

    Update a function with slice() method

--- 25. Confirm Ending/script.js
+++ 25. Confirm Ending/script.js
@@ -1,6 +1,6 @@
-const confirmEnding = (str, target) => {
-  let targetLength = target.length;
+const confirmEnding = (str, target) => { // 2 inputs string, target
+  let targetLength = target.length; 
   let ending = str.slice(-targetLength);
-  return ending === target;
+  return ending === target; //match ending, targeted string
 }
 
