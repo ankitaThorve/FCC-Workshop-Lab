6cc538e8612223aba63951e16e7e26db54d01574
Ankieta <ankietathorve@gmail.com>
2026-02-24 13:00:11 +0530

    Use methods split(), reverse() & join()

---
+++ 40. String Inverter.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    let reverse = str.split("").reverse().join("");
+    return reverse;
+}
+
+console.log(reverseString("hello"))
+console.log(reverseString("Howdy"))
+console.log(reverseString("Greetings from Earth"))
+console.log()
+console.log()
+console.log()
+console.log()
