a0309764f3baa1ddcca634983804ce0780ceb891
Ankieta <ankietathorve@gmail.com>
2026-03-06 18:40:44 +0530

    Function to remove product

--- 48. Inventory Management Prog.js
+++ 48. Inventory Management Prog.js
@@ -27,37 +27,37 @@ function addProduct(product) {
     }
 }
 
-/*
+
 
 // removeProduct function
 function removeProduct(productName, quantity) {
     const lowerName = productName.toLowerCase();
     const index = findProductIndex(lowerName);
+    //Product not present
+    // if (index === -1) {
+    //     console.log(lowerName + " not found");
+    //     return;
+    // }
 
-    if (index === -1) {
-        console.log(lowerName + " not found");
-        return;
-    }
+    // const currentQuantity = inventory[index].quantity;
+    // Not enough quantity
+    // if (currentQuantity < quantity) {
+    //     console.log(
+    //         "Not enough " +
+    //             lowerName +
+    //             " available, remaining pieces: " +
+    //             currentQuantity,
+    //     );
+    //     return;
+    // }
+    //Subtract quantity
+    // inventory[index].quantity -= quantity;
 
-    const currentQuantity = inventory[index].quantity;
-
-    if (currentQuantity < quantity) {
-        console.log(
-            "Not enough " +
-                lowerName +
-                " available, remaining pieces: " +
-                currentQuantity,
-        );
-        return;
-    }
-
-    inventory[index].quantity -= quantity;
-
-    console.log(
-        "Remaining " + lowerName + " pieces: " + inventory[index].quantity,
-    );
-
-    if (inventory[index].quantity === 0) {
-        inventory.splice(index, 1);
-    }
-} */
+    // console.log(
+    //     "Remaining " + lowerName + " pieces: " + inventory[index].quantity,
+    // );
+    // //Remove if zero
+    // if (inventory[index].quantity === 0) {
+    //     inventory.splice(index, 1);
+    // }
+} 
