c8ff5d225ecb05042199cd2a8ee2a61584e7a92b
Ankieta <ankietathorve@gmail.com>
2026-03-11 15:28:23 +0530

    Array arguments total sum

---
+++ 51. SumAll Numbers.js
@@ -0,0 +1,14 @@
+function sumAll(arr) {
+    let min = Math.min(arr[0], arr[1]);
+    let max = Math.max(arr[0], arr[1]);
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumAll([4, 1]));
+console.log(sumAll([5, 10]));
