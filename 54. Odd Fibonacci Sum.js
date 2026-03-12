69a1da18bc7ceb27edd2000921995bc2a53eb09a
Ankieta <ankietathorve@gmail.com>
2026-03-12 13:27:41 +0530

    Odd Fibonacci Sum calculator

---
+++ 54. Odd Fibonacci Sum.js
@@ -0,0 +1,22 @@
+function sumFibs(num) {
+    let prev = 0;
+    let curr = 1;
+    let sum = 0;
+
+    while (curr <= num) {
+        if (curr % 2 !== 0) {
+            sum += curr;
+        }
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return sum;
+}
+
+console.log(sumFibs(1));
+console.log(sumFibs(1000));
+console.log(sumFibs(4000000));
+console.log(sumFibs(4));
+console.log(sumFibs(75024));
+console.log(sumFibs(75025));
